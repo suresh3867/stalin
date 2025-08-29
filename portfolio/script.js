6ff9c83384f63a43043bd8f08f8577b54da201db
suresh <sureshchoudharyredmi10@gmail.com>
2025-08-29 13:40:05 +0530

    portfolio webpage

---
+++ portfolio/script.js
@@ -0,0 +1,25 @@
+// VANTA.CLOUDS2({
+//   el: "#clouds2",
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.0,
+//   minWidth: 200.0,
+//   scale: 1.0,
+//   cloudColor: 0x213e7c,
+//   texturePath: "./gallery/noise.png",
+// });
+
+VANTA.FOG({
+  el: "#fog",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  highlightColor: 0x0,
+  midtoneColor: 0xfcfafa,
+  lowlightColor: 0x1d1c1a,
+  blurFactor: 0.33,
+  speed: 0.70
+})
